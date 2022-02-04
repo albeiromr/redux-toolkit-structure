4894f630f06751475a8f188005f34eb906c61900
Albeiro Marìn <xlandru@hotmail.com>
2022-02-04 10:08:51 -0500

    Finaliza componente de todo

--- src/components/todo/todo.component.tsx
+++ src/components/todo/todo.component.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { AsyncStatusEnum } from '../../enums/async-status.enum';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import { getTodoByIdThunk } from '../../redux/todo/thunks/getTodoByIdyThunk';
 import './todo.component.scss';
@@ -23,12 +24,20 @@ const TodoComponent = () => {
     return (
         <div className='todo'>
             <h2 className='todo__title'>Todo</h2>
-            <ul>
-                <li className='todo__property'>{value.userId}</li>
-                <li className='todo__property'>{value.id}</li>
-                <li className='todo__property'>{value.title}</li>
-                <li className='todo__property'>{value.completed ? "Todo is completed" : "Todo is not completed"}</li>
-            </ul>
+
+            {status === AsyncStatusEnum.idle && <h3 className='todo__status-title'>Por favor busque un Todo</h3>}
+            {status === AsyncStatusEnum.loading && <h3 className='todo__status-title'>....Loading</h3>}
+            {status === AsyncStatusEnum.rejected && <h3 className='todo__status-title'>El Todo que buscas no existe</h3>}
+
+            {status === AsyncStatusEnum.completed && 
+                <ul>
+                    <li className='todo__property'>{value.userId}</li>
+                    <li className='todo__property'>{value.id}</li>
+                    <li className='todo__property'>{value.title}</li>
+                    <li className='todo__property'>{value.completed ? "Todo is completed" : "Todo is not completed"}</li>
+                </ul>
+            }
+
             <form onSubmit={handleFormSubmit} className='todo__form'>
                 <input type="number" className='todo__form-input' onChange={handleInputChange}/>
                 <button type='submit'className='todo__form-button'>Buscar</button>
